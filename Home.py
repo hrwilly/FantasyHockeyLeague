f4f016e04f0f64d981f204faa18efcf77d151deb
Hannah Willy <hrwilly@bu.edu>
2025-09-29 22:21:59 -0400

    home notes

--- Home.py
+++ Home.py
@@ -7,8 +7,21 @@ st.write("Welcome to the league!")
 st.write("Use the sidebar to navigate between pages.")
 
 st.subheader("Scoring Breakdown:")
-st.write("Goals: 2pts each/n"+
-          "Assists: 1pt each")
+st.write("Goals: 2 pts each")
+st.write("Assists: 1 pt each")
+st.write("Shots: 0.1 pts each")
+st.write("Penalty minutes: -0.5 pts each")
+st.write("Game Winning Goals: 1 pt each")
+st.write("Power Play Goals: 0.5 pts each")
+st.write("Short Handed Goals: 0.5 pts each")
+st.write("+/-: 0.5 pts each")
+st.write("Faceoffs Won: 0.1 pts each")
+st.write("Faceoffs Lost: -0.1 pts each")
+st.write("Blocked Shots: 0.5 pts each")
+st.write("Wins (for goalies): 4 pts each")
+st.write("Goals Against: -2 pts each")
+st.write("Saves: 0.2 pts each")
+st.write("Shutouts: 3 pts each")
 
 st.subheader("Rules:")
 
