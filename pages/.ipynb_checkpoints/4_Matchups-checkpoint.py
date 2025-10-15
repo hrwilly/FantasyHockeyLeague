72319bc69b0a41ea243d1ce4686ca47c3c34d66b
Hannah Willy <hrwilly@bu.edu>
2025-10-14 21:26:35 -0400

    matchup edits

--- pages/.ipynb_checkpoints/4_Matchups-checkpoint.py
+++ pages/.ipynb_checkpoints/4_Matchups-checkpoint.py
@@ -22,16 +22,16 @@ week_matchups["matchup_label"] = week_matchups.apply(
     lambda row: f"{row['home_team']} vs {row['away_team']}", axis=1
 )
 
-    # --- Dropdown to select matchup ---
-    selected_matchup_label = st.selectbox(
+# --- Dropdown to select matchup ---
+selected_matchup_label = st.selectbox(
         "Select Matchup",
         week_matchups["matchup_label"]
-    )
+)
 
-    # --- Get the selected matchup row ---
-    selected_matchup = week_matchups[
+# --- Get the selected matchup row ---
+selected_matchup = week_matchups[
         week_matchups["matchup_label"] == selected_matchup_label
-    ].iloc[0]
+].iloc[0]
 
-    st.write(f"### {selected_matchup['home_team']} 🏠 vs {selected_matchup['taway_team']} 🧳")
-    st.write(f"**Scores:** {selected_matchup['home_team_points']} - {selected_matchup['away_team_points']}")
+st.write(f"### {selected_matchup['home_team']} 🏠 vs {selected_matchup['taway_team']} 🧳")
+st.write(f"**Scores:** {selected_matchup['home_team_points']} - {selected_matchup['away_team_points']}")
