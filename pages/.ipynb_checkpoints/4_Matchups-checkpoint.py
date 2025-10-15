a2c1dcb358c4168c9325bc6d84328afe1089103b
Hannah Willy <hrwilly@bu.edu>
2025-10-14 21:34:21 -0400

    matchup edits

--- pages/.ipynb_checkpoints/4_Matchups-checkpoint.py
+++ pages/.ipynb_checkpoints/4_Matchups-checkpoint.py
@@ -5,11 +5,19 @@ st.title("📅 Matchups")
 
 # --- Load matchups ---
 matchups_df = db_utils.load_matchups()
+managers = db_utils.load_teams()
+
+matchups_df = (
+        matchups_df
+        .merge(managers.rename(columns={"team_name": "home_team", "manager": "manager_1"}), on="home_team")
+        .merge(teams_df.rename(columns={"team_name": "away_team", "manager": "manager_2"}), on="away_team")
+    )
+
 
 selected_week = st.selectbox("Select week", sorted(matchups_df["week"].unique()))
 week_matchups = matchups_df[matchups_df["week"] == selected_week]
 
-st.dataframe(week_matchups.set_index('week'))
+st.dataframe(week_matchups.set_index('week').drop(['manager_1', 'manager_2'], axis = 1))
 
 weeks = sorted(matchups_df["week"].unique())
 
@@ -30,4 +38,5 @@ selected_matchup = week_matchups[
 ].iloc[0]
 
 st.write(f"### {selected_matchup['home_team']} vs {selected_matchup['away_team']}")
+st.write(f"# {selected_matchup['manager_1']} vs {selected_matchup['manager_2']}")
 st.write(f"**Scores:** {selected_matchup['home_team_points']} - {selected_matchup['away_team_points']}")
