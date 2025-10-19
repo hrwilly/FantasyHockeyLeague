32a6861ed580a5b20adc9b01117d2c3588b6bf29
Hannah Willy <hrwilly@bu.edu>
2025-10-19 18:11:12 -0400

    standings edits

--- pages/.ipynb_checkpoints/6_Standings-checkpoint.py
+++ pages/.ipynb_checkpoints/6_Standings-checkpoint.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import db_utils
 
-st.title("Weekly Fantasy Points & Standings")
+st.title("Standings")
 
 standings = db_utils.load_teams()
 
-st.dataframe(standings[['Place', 'team_name', 'manager', 'W', 'L', 'PF', 'PA']], hide_index = True, height = 455)
+st.dataframe(standings[['Place', 'team_name', 'manager', 'W', 'L', 'PF', 'PA']], hide_index = True, height = 460)
